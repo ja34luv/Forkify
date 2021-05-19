f6df7d8e81546d040d3f8720c592f82fc10bbbd4
Jay <ja34luv@gmail.com>
2021-05-19 14:11:31 -0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7a352089-5563-4abb-92a0-ecc9575fb342';
+export const MODAL_CLOSE_SEC = 2.5;
